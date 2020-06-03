a796afd8f1e6bc2417ad0460489623a926dbd498
eudald <eudald.elias@gmail.com>
2020-06-03 13:35:42 +0200

    next try

--- backend/config.py
+++ backend/config.py
@@ -6,16 +6,16 @@ class ProductionConfig(Config):
     DEBUG = False
     SQLALCHEMY_DATABASE_URI = config('DATABASE_URL', default='localhost')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    STATIC_FOLDER = "../frontend/dist/static"
-    TEMPLATE_FOLDER = "../frontend/dist"
+    STATIC_FOLDER = "/static"
+    TEMPLATE_FOLDER = "/templates"
     SECRET_KEY = config('SECRET_KEY', default='localhost')
 
 class DevelopmentConfig(Config):
     DEBUG = True
     SQLALCHEMY_DATABASE_URI = 'sqlite:///data.db'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    STATIC_FOLDER = "../frontend/dist/static"
-    TEMPLATE_FOLDER = "../frontend/dist"
+    STATIC_FOLDER = "/static"
+    TEMPLATE_FOLDER = "/templates"
     SECRET_KEY = "kdsfklsmfakfmafmadslvsdfasdf"
 
 config = {
